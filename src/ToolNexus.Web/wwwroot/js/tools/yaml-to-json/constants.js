69bdc2846b5c83a94d6cf585a399a2cc6b3aa1c1
Ankit Kumar <ankit.dsmb@gmail.com>
2026-02-20 02:31:13 +0530

    Refactor YAML to JSON tool into modular safe client converter

---
+++ src/ToolNexus.Web/wwwroot/js/tools/yaml-to-json/constants.js
@@ -0,0 +1,12 @@
+export const LARGE_INPUT_BYTES = 180_000;
+export const PROCESSING_SPINNER_MIN_MS = 120;
+export const JSON_INDENT_DEFAULT = 2;
+export const POLLUTION_KEYS = new Set(['__proto__', 'constructor', 'prototype']);
+
+export const DEFAULT_STATS = {
+  lines: 0,
+  bytes: 0,
+  objects: 0,
+  arrays: 0,
+  primitives: 0
+};
